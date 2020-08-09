23c6b64e02ea6a37d7051150a86987fa3512a517
Pham Cao Dai <daikadai1026@gmail.com>
2020-08-09 13:01:16 +0700

    3. Refactoring

--- app/screens/ViewImageScreen.js
+++ app/screens/ViewImageScreen.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Image, StyleSheet, View } from 'react-native'
+import color from '../config/color'
 
 const ViewImageScreen = () => {
   return (
@@ -15,19 +16,19 @@ const styles = StyleSheet.create({
   closeIcon: {
     width: 50,
     height: 50,
-    backgroundColor: '#fc5c65',
+    backgroundColor: color.primary,
     position: 'absolute',
     top: 40,
     left: 30
   },  
   container: {
-    backgroundColor: '#000',
+    backgroundColor: color.black,
     flex: 1
   },
   deleteIcon: {
     width: 50,
     height: 50,
-    backgroundColor: '#4ecdc4',
+    backgroundColor: color.secondary,
     position: 'absolute',
     top: 40,
     right: 30
