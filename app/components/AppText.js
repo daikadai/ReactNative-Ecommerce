959016483fe57d8862a5ac91a1120e769ebdff02
Pham Cao Dai <daikadai1026@gmail.com>
2020-08-09 16:51:33 +0700

    4. Building Custom Button Component

---
+++ app/components/AppText.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+import styles from './AppText/styles'
+
+const AppText = ({ children }) => {
+  return (
+    <Text>{children}</Text>
+  )
+}
+
+
+
+
+export default AppText
