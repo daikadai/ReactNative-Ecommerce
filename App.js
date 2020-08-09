12ae956c01a05221b887b37fb090fa2f76c8d734
Pham Cao Dai <daikadai1026@gmail.com>
2020-08-09 17:53:06 +0700

    8. Building the ListItem Component

--- App.js
+++ App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import { View } from 'react-native';
-import Card from './app/components/Card';
 import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
 
 
--- app/screens/ListingDetailsScreen.js
+++ app/screens/ListingDetailsScreen.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { View, Image, StyleSheet } from 'react-native'
 import AppText from '../components/AppText'
 import colors from '../config/colors'
+import ListItem from '../components/ListItem'
 
 const ListingDetailsScreen = () => {
   return (
@@ -10,6 +11,13 @@ const ListingDetailsScreen = () => {
      <View style={styles.detailContainer}>
       <AppText style={styles.title}>Red jacket for sale</AppText>
       <AppText style={styles.price}>$100</AppText>
+      <View style={styles.userContainer}>
+        <ListItem
+          image={require("../assets/mosh.jpg")}
+          title="Mosh Hamedani"
+          subTitle="5 Listings"
+        />
+      </View>
      </View>
    </View>
   )
@@ -32,6 +40,9 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 24,
     fontWeight: '500'
+  },
+  userContainer: {
+    marginVertical: 40
   }
 })
 
