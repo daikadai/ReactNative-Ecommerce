4d63ab3c66700e409dd9258d600cd51681365bce
Pham Cao Dai <daikadai1026@gmail.com>
2020-08-10 00:45:15 +0700

    16. Implementing Pull to Refresh

--- app/components/Screen.js
+++ app/components/Screen.js
@@ -11,7 +11,8 @@ const Screen = ({ children }) => {
 
 const styles = StyleSheet.create({
   screen: {
-    paddingTop: Constants.statusBarHeight
+    paddingTop: Constants.statusBarHeight,
+    flex: 1
   }
 })
 
