567100cede57823bf12930abc8a198d21441d9d9
Pham Cao Dai <daikadai1026@gmail.com>
2020-08-23 15:20:07 +0700

    17. Building the icon Component

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import MessagesScreen from './app/screens/MessagesScreen';
+import Screen from './app/components/Screen';
+import Icon from './app/components/Icon';
 
 
 export default function App() {
   return (
-    <MessagesScreen />
+    <Screen>
+      <Icon
+          name="email"
+          size={100}
+          backgroundColor='red'
+          iconColor="white"
+       />
+    </Screen>
   );
 }
 
