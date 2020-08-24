5f8e3388b8d723f1f58d02345630e3b6c8f3639a
Pham Cao Dai <daikadai1026@gmail.com>
2020-08-24 10:52:32 +0700

    21. Text Input

--- App.js
+++ App.js
@@ -1,9 +1,22 @@
-import React from 'react';
-import ListingsScreen from './app/screens/ListingsScreen';
+import React, { useState } from 'react';
+import Screen from './app/components/Screen';
+import { TextInput, Text } from 'react-native';
 
 export default function App() {
+  const [firstName, setFirstName] = useState('')
   return (
-    <ListingsScreen />
+   <Screen>
+      <Text>{firstName}</Text>
+      <TextInput 
+        secureTextEntry={true}
+        onChangeText={text => setFirstName(text)}
+        placeholder="First Name"
+        style={{
+          borderBottomColor: "#ccc",
+          borderBottomWidth: 1
+        }}
+        />
+   </Screen>
   );
 }
 
