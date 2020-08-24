41b1621c7e215fc630898c03c3bf2497828bfaa7
Pham Cao Dai <daikadai1026@gmail.com>
2020-08-24 15:58:15 +0700

    31. The Touched State

--- app/components/ErrorMessage.js
+++ app/components/ErrorMessage.js
@@ -2,8 +2,9 @@ import React from 'react'
 import AppText from './AppText'
 import { StyleSheet } from 'react-native';
 
-const ErrorMessage = ({ error }) => {
-  if(!error) return null;
+const ErrorMessage = ({ error, visible }) => {
+  if(!visible || !error) return null;
+  console.log(visible);
   return (
     <AppText style={styles.error}>
       {error}
