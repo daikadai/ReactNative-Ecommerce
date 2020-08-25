15e8752d47ca938ce8c5dc060c6b5507cbfc4e70
Pham Cao Dai <daikadai1026@gmail.com>
2020-08-25 17:13:17 +0700

    48. Building FormImagePicker & Improving ListingEditScreem

--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -4,12 +4,14 @@ import { StyleSheet } from 'react-native'
 import { AppForm, AppFormField, SubmitButton, AppFormPicker } from '../components/forms'
 import * as Yup from 'yup';
 import CategoryPickerItem from '../components/CategoryPickerItem';
+import FormImagePicker from '../components/forms/FormImagePicker';
 
 const validationSchema = Yup.object().shape({
   title: Yup.string().required().min(1).label('Title'),
   price: Yup.number().required().min(1).max(10000).label('Price'),
   description: Yup.string().label('Description'),
-  category: Yup.object().required().nullable().label('Category')
+  category: Yup.object().required().nullable().label('Category'),
+  images: Yup.array().min(1, "Please select at least one image")
 })
 
 const categories = [
@@ -77,11 +79,13 @@ const ListingEditScreen = () => {
           title: '',
           price: '',
           description: '',
-          category: null
+          category: null,
+          images: []
         }}
         onSubmit={values => console.log(values)}
         validationSchema={validationSchema}
       >
+        <FormImagePicker name="images"/>
         <AppFormField maxLength={255} name="title" placeholder="Title"/>
         <AppFormField
           keyboardType="numeric"
