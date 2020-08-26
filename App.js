a4ab30fc5458ce4120ba952ef60e6a939ff10838
Pham Cao Dai <daikadai1026@gmail.com>
2020-08-26 12:18:14 +0700

    51. Building AuthNavigator

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
-import React, { useState } from 'react';
-import ListingEditScreen from './app/screens/ListingEditScreen';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import AuthNavigator from './app/navigation/AuthNavigator';
 
 export default function App() {
-  return <ListingEditScreen />
+  return (
+    <NavigationContainer>
+      <AuthNavigator />
+    </NavigationContainer>
+  )
 }
 
--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { ImageBackground, StyleSheet, View, Image, Text } from 'react-native'
 import AppButton from '../components/AppButton'
 
-const WelcomeScreen = () => {
+const WelcomeScreen = ({ navigation }) => {
   return (
     <ImageBackground blurRadius={10} style={styles.background} source={require("../assets/background.jpg")}>
       <View style={styles.logoContainer}>
@@ -10,8 +10,8 @@ const WelcomeScreen = () => {
         <Text style={styles.tagline}>Sell What You Don't Need</Text>
       </View>
       <View style={styles.buttonContainer}>
-        <AppButton title="Login" />
-        <AppButton title="Register" color="secondary"/>
+        <AppButton title="Login" onPress={() => navigation.navigate('Login')}/>
+        <AppButton title="Register" color="secondary" onPress={() => navigation.navigate('Register')}/>
       </View>
     </ImageBackground>
   )
