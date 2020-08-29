09e01ebe9f41b1bcfb5d9c9d828f215e870f2be3
Pham Cao Dai <daikadai1026@gmail.com>
2020-08-29 19:01:53 +0700

    78. Controlling the Splash Screen

--- App.js
+++ App.js
@@ -7,19 +7,18 @@ import AuthContext from './app/auth/context';
 import AppNavigator from './app/navigation/AppNavigator';
 import authStorage from './app/auth/storage';
 import jwtDecode from 'jwt-decode';
+import { AppLoading } from 'expo';
 
 export default function App() {
   const [ user, setUser ] = useState();
-
+  const [isReady, setIsReady] = useState(false);
   const resotreToken = async () => {
     const token = await authStorage.getToken();
     if(!token) return;
     setUser(jwtDecode(token));
   }
 
-  useEffect(() => {
-    resotreToken();
-  },[]);
+  if(!isReady) return <AppLoading startAsync={resotreToken} onFinish={() => setIsReady(true)}/>
 
   return (
    <AuthContext.Provider value={{user, setUser}}>
